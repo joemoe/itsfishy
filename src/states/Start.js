ff4e393f5888670fd078b2b67b93bd5b66c860b0
Johannes Moser HM <sofa@morgenstille.at>
2015-02-21 12:14:28 +0100

    awesome new illustrations

--- src/states/Start.js
+++ src/states/Start.js
@@ -1,13 +1,21 @@
 'use strict';
 
+var config = require('../components/Configuration.js');
+
 var Start = function () {
 };
 
 Start.prototype = {
     create: function () {
+        var bg = this.game.add.tileSprite(
+            0, 0, config.gameWidth, config.gameHeight, 'background'
+        );
+        var startscreen = this.game.add.sprite(
+            0, 0, 'startscreen'
+        );
         var playButton = this.game.add.button(
-            this.game.world.centerX,
-            this.game.world.centerY,
+            650,
+            450,
             'play',
             this.playItsFishy,
             this
